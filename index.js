f49c0e1f685578ef5ec27ce275e937d26ee4033b
t-powers <127955989+t-powers@users.noreply.github.com>
2024-09-03 20:42:15 -0400

    Created HTML, config db

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-const firebaseConfig = {}
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
+
+const firebaseConfig = {
+  databaseURL: "https://leads-tracker-app-d7fa4-default-rtdb.firebaseio.com/",
+};
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+console.log(database);

0ed2e70881948678401f4613f2004b2e058c7745
t-powers <127955989+t-powers@users.noreply.github.com>
2024-09-04 21:18:01 -0400

    Updated js

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+deleteBtn.addEventListener("dblclick", function () {});
+
+inputBtn.addEventListener("click", function () {
+  console.log(inputEl.value);
+  inputEl.value = "";
+});

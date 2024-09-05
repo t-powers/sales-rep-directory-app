4be21bdf117bf4e4a513fa275f188a9c0e089bdc
t-powers <127955989+t-powers@users.noreply.github.com>
2024-09-04 21:23:53 -0400

    Add db reference

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 
 const firebaseConfig = {
   databaseURL: "https://leads-tracker-app-d7fa4-default-rtdb.firebaseio.com/",
 };
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const referenceInDB = ref(database, "leads");
 
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
